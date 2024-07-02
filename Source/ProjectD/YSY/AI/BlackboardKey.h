bf505db829765c22596fecdf95706d8dad60f92e
yoru4890 <137769028+yoru4890@users.noreply.github.com>
2024-07-02 17:41:56 +0900

    [ADD] TraceChannel, BlackboardKey Macro

---
+++ Source/ProjectD/YSY/AI/BlackboardKey.h
@@ -0,0 +1,5 @@
+#pragma once
+
+#define BBKEY_RANGE TEXT("bRange")
+#define BBKEY_TARGET TEXT("Target")
+#define BBKEY_RANDOMLOCATION TEXT("RandomLocation")
---
+++ Source/ProjectD/YSY/Collision/CollisionChannel.h
@@ -0,0 +1,9 @@
+
+#pragma once
+
+#define CCHANNEL_BUILDINGTRACE ECollisionChannel::ECC_EngineTraceChannel3
+#define CCHANNEL_PLAYERDAMAGE ECollisionChannel::ECC_EngineTraceChannel4
+
+#define TCHANNEL_BUILDINGTRACE ETraceTypeQuery::TraceTypeQuery3
+#define TCHANNEL_PLAYERDAMAGE ETraceTypeQuery::TraceTypeQuery4
+
