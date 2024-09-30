0867734d2898299b593889285d5a3b3dcb8e356f
Orblangjeee <wjddns0130@gmail.com>
2024-09-30 18:07:09 +0900

    [ADD] SoundManager

---
+++ Source/ProjectD/LJW/Manager/DDSoundManager.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/NoExportTypes.h"
+#include "DDSoundManager.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PROJECTD_API UDDSoundManager : public UObject
+{
+	GENERATED_BODY()
+	
+};
