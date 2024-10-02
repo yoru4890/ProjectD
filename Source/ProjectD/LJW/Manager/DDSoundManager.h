0ff2007903234c406304211ed8345bc4edd6d8c5
Orblangjeee <wjddns0130@gmail.com>
2024-10-02 19:10:23 +0900

    [UPDATE] SoundManager

--- Source/ProjectD/LJW/Manager/DDSoundManager.h
+++ Source/ProjectD/LJW/Manager/DDSoundManager.h
@@ -1,9 +1,10 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
 #pragma once
 
 #include "CoreMinimal.h"
 #include "UObject/NoExportTypes.h"
+#include "Sound/SoundBase.h"
+#include "Components/AudioComponent.h"
+#include "Kismet/GameplayStatics.h"
 #include "DDSoundManager.generated.h"
 
 
@@ -13,4 +14,27 @@ class PROJECTD_API UDDSoundManager : public UObject
 {
 	GENERATED_BODY()
 	
+public:
+	UDDSoundManager();
+
+	void Initialize();
+
+	UFUNCTION(BlueprintCallable, Category = "BGM")
+	void PlayBGM(USoundBase* PlayingBGM);
+
+	UFUNCTION(BlueprintCallable, Category = "BGM")
+	void StopBGM();
+	
+public:
+	
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "BGM")
+	TObjectPtr<class USoundBase> StartBGM;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "BGM")
+	TObjectPtr<class USoundBase> InGameBGM;
+
+protected:
+	UPROPERTY()
+	TObjectPtr<class UAudioComponent> AudioComponent;
+
 };
